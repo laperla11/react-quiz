fafbe1e1fcc6bdcb8998b05edc8d45332003e24f
Kadir <kegilmez@gmail.com>
2019-05-07 16:54:51 +0100

    Kadir react-quiz

--- src/Question6-ImageGallery.js
+++ src/Question6-ImageGallery.js
@@ -14,16 +14,45 @@ class ImageGallery extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgSrc: null
+      imgSrc: null,
+      isLoading: true
     };
   }
 
-  fetchData = () => {
-    fetch().then(res => res.json());
-  };
+  componentDidMount() {
+    fetch("https://auspicious-baritone.glitch.me/gorilla")
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          return res;
+        } else {
+          throw new Error("HTTP error");
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        this.setState({
+          isLoading: false,
+          imgSrc: data
+        });
+      })
+      .catch(err => {
+        this.setState({
+          isLoading: false,
+          error: err
+        });
+      });
+    console.log(this.state.err);
+  }
 
   render() {
-    return <img src={this.state.imgSrc} alt="An animal" />;
+    if (this.state.isLoading) {
+      return <span>Loading... 👽</span>;
+    } else if (this.state.error) {
+      return <span>Something went wrong 😭</span>;
+    } else {
+      return <img src={this.state.imgSrc} alt="An animal" />;
+    }
   }
 }
 
